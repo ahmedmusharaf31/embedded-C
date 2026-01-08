bc2ca6f9f8a2aa17d4768fb40beaeae32a2afd67
Ahmed Musharaf <ahmed.musharaf31@gmail.com>
2026-01-08 12:26:44 +0500

    qwerty

--- new.c
+++ new.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #define OPERATION_MODE 1
 
+/* Preprocessor Directives */
+
 int startMode_1(void){
     return 0;
 }
@@ -9,19 +11,58 @@ int startMode_2(void){
     return 0;
 }
 
-int main(void){
+// int main(void){
 
-    #if(OPERATION_MODE == 1)
-        startMode_1();
-    #elif(OPERATION_MODE == 2)
-        startMode_2();
+//     #if(OPERATION_MODE == 1)
+//         startMode_1();
+//     #elif(OPERATION_MODE == 2)
+//         startMode_2();
     
-    #else
-        #error OPERATION_MODE must be 1 or 2 
+//     #else
+//     /* Display error message and stop process */
+//     #error OPERATION_MODE must be 0 or 1 
 
+//     #endif
+    
+//     return 0;
+// }
+
+
+/* Code Compilation Process */
+
+#define IN_DEBUG_MODE 0
+// for gcc compiler:
+#pragma optimize("",off)
+
+// for ghs compiler:
+#pragma ghs section ".bss"=".mySection"
+int myArray[1000]; // it will put this array in section called ".mySection" in memory
+#pragma ghs section
+
+#define MAX(a,b) ((a)>(b))?(a):(b)
+
+// void testFunction(void){
+//     #ifdef IN_DEBUG_MODE
+//         printf("DEBUG: entered testFunction\n");
+//     #endif
+// }
+
+void testFunction(void){
+    #ifdef IN_DEBUG_MODE
+        printf("DEBUG: entered testFunction\n");
     #endif
 
-    printf("Done!\n\n"); 
-    
+    #undef IN_DEBUG_MODE
+
+    #ifdef IN_DEBUG_MODE
+        printf("DEBUG: still in debug mode\n");
+    #endif
+
+}
+
+signed main(int argc, char **argv){
+    testFunction();
+
+    printf("max value is: %d\n\n",MAX(5,10));
     return 0;
 }

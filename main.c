f66a5f857cd2f44897e25a72bbe4d003afc3a8e2
Ahmed Musharaf <ahmed.musharaf31@gmail.com>
2026-01-11 14:52:49 +0500

    cmd operations

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "max.h"
+
+int main(){
+    printf("maximum is %d\n",max(10,20));
+    return 0;
+}
---
+++ max.h
@@ -0,0 +1,5 @@
+#ifndef MAX_H_INCLUDED
+
+#define MAX_H_INCLUDED
+    int max(int num1, int num2);
+#endif
